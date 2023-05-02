8e0ae61bb03087c74befb9f1892e43e9e368b233
Arjun Esguerra <arjundesguerra@gmail.com>
2023-05-02 01:15:48 -0700

    added specific variables to appointment subclasses

--- src/Models/SalesAppointment.java
+++ src/Models/SalesAppointment.java
@@ -3,9 +3,13 @@ package Models;
 import java.time.LocalDateTime;
 
 public class SalesAppointment extends Appointment {
+    private String tradeInVehicle;
+    private String financingOptions;
 
-
-    public SalesAppointment(int appointmentID, String appointmentTitle, String appointmentDescription, String appointmentLocation, String appointmentType, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID, int userID, int contactID) {
+    public SalesAppointment(int appointmentID, String appointmentTitle, String appointmentDescription, String appointmentLocation, String appointmentType, String tradeInVehicle, String financingOptions, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID, int userID, int contactID) {
         super(appointmentID, appointmentTitle, appointmentDescription, appointmentLocation, appointmentType, startDateTime, endDateTime, customerID, userID, contactID);
+        this.tradeInVehicle = tradeInVehicle;
+        this.financingOptions = financingOptions;
     }
+
 }
--- src/Models/ServiceAppointment.java
+++ src/Models/ServiceAppointment.java
@@ -3,9 +3,17 @@ package Models;
 import java.time.LocalDateTime;
 
 public class ServiceAppointment extends Appointment {
+    private String serviceType;
+    private double serviceCost;
 
-
-    public ServiceAppointment(int appointmentID, String appointmentTitle, String appointmentDescription, String appointmentLocation, String appointmentType, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID, int userID, int contactID) {
+    public ServiceAppointment(int appointmentID, String appointmentTitle, String appointmentDescription, String appointmentLocation, String appointmentType, String serviceType, double serviceCost, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID, int userID, int contactID) {
         super(appointmentID, appointmentTitle, appointmentDescription, appointmentLocation, appointmentType, startDateTime, endDateTime, customerID, userID, contactID);
+        this.serviceType = serviceType;
+        this.serviceCost = serviceCost;
     }
+
+    public String getServiceType() { return serviceType; }
+
+    public double getServiceCost() { return serviceCost; }
+
 }
