e173021b3bc7b63784b1d3deba92608963727c8e
Arjun Esguerra <arjundesguerra@gmail.com>
2023-05-02 01:03:25 -0700

    created sales and service appointment classes

---
+++ src/Models/SalesAppointment.java
@@ -0,0 +1,11 @@
+package Models;
+
+import java.time.LocalDateTime;
+
+public class SalesAppointment extends Appointment {
+
+
+    public SalesAppointment(int appointmentID, String appointmentTitle, String appointmentDescription, String appointmentLocation, String appointmentType, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID, int userID, int contactID) {
+        super(appointmentID, appointmentTitle, appointmentDescription, appointmentLocation, appointmentType, startDateTime, endDateTime, customerID, userID, contactID);
+    }
+}
